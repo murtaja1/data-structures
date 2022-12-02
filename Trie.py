02ebb0d8882fed41f3f6ea9223d507c0f2d1436d
murtaja1 <murtajaadnan7@gmail.com>
2022-12-03 01:42:38 +0300

    Upgrade: add methods to Trie data structure

--- Trie.py
+++ Trie.py
@@ -5,9 +5,9 @@
     # 3. every node stores link of the next character of the string.
     # 4. every node keeps track of 'end of string'.
 
-# why use Trie:
-    # -spelling checker.
-    # -auto completion.
+# when to use Trie:
+    # - spelling checker.
+    # - auto completion.
 
 class TrieNode:
     def __init__(self) -> None:
@@ -18,4 +18,32 @@ class Trie:
     def __init__(self) -> None:
         self.root = TrieNode()
 
+    # check visualization\Trie\Trie Insertion.png
+    # time complexity is O(n) where n is the length of the word.
+    # space complexity is O(n)
+    def insertNode(self, word):
+        current = self.root
+        for char in word:
+            node = current.children.get(char)
+            if not node:
+                node = TrieNode()
+                current.children.update({char:node}) 
+            current = node
+        current.endOfString = True
+        return "Successfully Inserted!"
+
+    # time complexity is O(n) where n is the length of the word.
+    # space complexity is O(1)
+    def searchNode(self, word): 
+        current = self.root
+        for char in word:
+            node = current.children.get(char)
+            if not node:
+                return False
+            current = node
+        return current.endOfString
+
 newTrie = Trie()
+print(newTrie.insertNode('h'))
+# print(newTrie.insertNode('hhl'))
+# print(newTrie.searchNode('hell'))
