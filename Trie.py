3b15c720504949bccf68b9b276c184be7f1ae939
murtaja1 <murtajaadnan7@gmail.com>
2022-12-04 17:30:56 +0300

    Upgrade: add delete method to Trie data structure

--- Trie.py
+++ Trie.py
@@ -43,7 +43,45 @@ class Trie:
             current = node
         return current.endOfString
 
+    # time complexity is O(n) where n is the length of the word.
+    # space complexity is O(n)
+    def deleteNode(self, root, word, index=0):
+        child = word[index]
+        currentNode = root.children.get(child)
+        canThisNodeBeDeleted = False
+        print(child, currentNode.children)
+        # case 1: some other's prefix of string is same as the one that we want to delete. ex: (API, APPLE). check visualization\Trie\deletion case 1.png
+        if len(currentNode.children) > 1:
+            print('case 1', child)
+            self.deleteNode(currentNode, word, index+1) # update the index to go to the next char.
+            return False # this can't be deleted.
+        # case 2: the string is a prefix of another string. ex: (API, APIS) check visualization\Trie\deletion case 2.png
+        if index == len(word)-1:
+            print('case 2', child)
+            if len(currentNode.children) >= 1: # if it has children more than 1.
+                currentNode.endOfString = False
+                return False
+            else:
+                print('case 2 last:', child)
+                root.children.pop(child)
+                return True
+        # case 3: other string is a prefix of this string. ex: (APIS, AP) check visualization\Trie\deletion case 3.png
+        if currentNode.endOfString == True:
+            print('case 3', child)
+            self.deleteNode(currentNode, word, index+1)
+            return False
+        print('aaaaaaaaaaaa')
+        # case 4: not any node depends on this string. check visualization\Trie\deletion case 4.png
+        canThisNodeBeDeleted = self.deleteNode(currentNode, word, index+1)
+        print('case 4', child, canThisNodeBeDeleted)
+        if canThisNodeBeDeleted:
+            root.children.pop(child)
+            return True
+        else:
+            return False
+
 newTrie = Trie()
-print(newTrie.insertNode('h'))
-# print(newTrie.insertNode('hhl'))
-# print(newTrie.searchNode('hell'))
+print(newTrie.insertNode('API'))
+print(newTrie.insertNode('APPLE'))
+newTrie.deleteNode(newTrie.root, 'API')
+print(newTrie.searchNode('API'))
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ $1 == am ]
   then 
     git add .
-    git commit -m 'Upgrade: add methods to Trie data structure'
+    git commit -m 'Upgrade: add delete method to Trie data structure'
 fi
 # stands for push 
 if [ $2 == p ]
