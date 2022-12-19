bb086a81dd8f06d5dd241e8d17d35cfbe940f2bb
murtaja1 <murtajaadnan7@gmail.com>
2022-12-19 22:51:36 +0300

    Upgrade: add sorting

---
+++ algorithoms/sorting/types.py
@@ -0,0 +1,23 @@
+# Type of Sorting:
+    # Space Used: 
+        # In Place: no need for extra space. ex: Bubble sort.
+        # Out of Place: uses extra space. ex: Merge sort
+    # Stability: check visualization\stability.png/stability2.png.
+        # Stable: if a sorting algorithm after sorting the contents does not change the sequence of similar content in which they appear, then it's called stable sorting. ex: Insertion sort.
+        # Unstable: if a sorting algorithm after sorting the contents changes the sequence of similar content in which they appear, then it's called unstable sorting. ex: Quick sort.
+
+# Sorting Terminology:
+    # Increasing Order: 
+        # - (ascending) If a successive element is greater than the previous one.
+        # - ex: 1, 2, 3, 4, 5,,,
+    # Decreasing Order:
+        # - (descending) If a successive element is less than the previous one.
+        # - ex: 5, 4, 3, 2, 1,,,,
+    # Non increasing Order:
+        # If successive element is less than or equal to its previous one.
+        # ex: 5, 4, 4, 3, 2, 1,,,,
+    # Non Decreasing Order:
+        # If successive element is greater than or equal to its previous one.
+        # ex: 1, 2, 3, 3, 4, 5,,,,
+
+# Which algorithm to choose: based on: 1- stability. 2- Space efficient. 3- time efficient.
