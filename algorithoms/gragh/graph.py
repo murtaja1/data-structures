238a09e8a2ef17a148b66393070b6bd6288b74ea
murtaja1 <murtajaadnan7@gmail.com>
2022-12-28 20:16:28 +0300

    Upgrade: add graph algorithm

---
+++ algorithoms/gragh/graph.py
@@ -0,0 +1,14 @@
+ # what is Graph: Graph consists of two vertices (nodes) and a set of edges connecting these vertices. check visualization\graph\graph.png
+
+ # Graph Terminologies:
+    # - Vertices (vertex): the nodes of the graph. check visualization\graph\graph.png
+    # - Edge: the line that connects the pairs of vertices. check visualization\graph\graph.png 
+    # - Unweighted Graph: does not have weights associated with any edge.
+    # - Weighted Graph: has weights associated with any edge. check visualization\graph\weightedGraph.png
+    # - Undirected Graph: does not have directions associated with any edge.
+    # - Directed Graph: has directions associated with any edge. check visualization\graph\directedGraph.png
+    # - Cyclic Graph: has at least one loop. check visualization\graph\cyclicGraph.png
+    # - Acyclic Graph: has no loop. check visualization\graph\acyclicGraph.png
+    # - Tree: it's a special case of directed acyclic graph. check visualization\graph\tree.png
+
+
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 if [ "$1" == am ]
   then 
     git add .
-    git commit -m 'Upgrade: add Linear and binary search'
+    git commit -m 'Upgrade: add graph algorithm'
   fi
 # stands for push 
 if [ "$2" == p ]
